e8aee15f9cddd300e2255ff916e307bd36c805da
rudaya28 <103644805+rudaya28@users.noreply.github.com>
2022-08-01 18:44:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var background,backgroundImg;
+
+
+
+
+
+
+
+
+
+function preload(){
+    backgroundImg.loadImage("background2.png");
+}
+
+
+
+
+
+
+function setup(){
+createcanavas(windowWidth,windowHeight);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function draw();
+background(0)
+drawsprite();
